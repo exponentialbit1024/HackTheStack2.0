726105a768bf74175de7d84289fa2255c55adb05
exponentialbit1024 <shah255@purdue.edu>
2017-02-17 13:21:18 -0500

    angular added

---
+++ competition_website/app/static/js/index.js
@@ -0,0 +1,12 @@
+var app = angular.module('hackTStack', ['ngMaterial'])
+  .config(function($mdThemingProvider){
+    $mdThemingProvider.theme('default')
+      .primaryPalette('blue')
+      .backgroundPalette('grey')
+      .accentPalette('blue-grey')
+      .warnPalette('red');
+  });
+
+app.controller("challengCtrl", function($scope){
+
+});
